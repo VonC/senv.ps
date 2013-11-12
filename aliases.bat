3d8d47928733a84342861b25e882ed4d565e998d
VonC <vonc@laposte.net>
2013-11-12 22:46:15 +0100

    Add bat, including senv.bat.

---
+++ aliases.bat
@@ -0,0 +1,7 @@
+doskey cdhugo=cd %PROG%\go\src\github.com\spf13\hugo
+doskey cdhugot=cd %PROG%\hugo\test
+doskey cdjazz=cd %PROG%\go\src\oslc\jazz
+doskey cdgit=cd %PROG%\git\git
+
+set GFW="%LOCALAPPDATA%\GitHub\GitHub.appref-ms"
+doskey g4w=%GFW% $*
---
+++ senv.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "& \"%prog%\git\senv\senv.ps1\"" %*
+call %PRGS%\setpath.bat
+
+copy "%prog%\git\senv\senv.bat" "%prgs%\bin" >NUL
+copy "%prog%\git\senv\alias.bat" "%prgs%\bin" >NUL
+copy "%prog%\git\senv\h.bat" "%prgs%\bin" >NUL
+copy "%prog%\git\senv\l.bat" "%prgs%\bin" >NUL
+
+if exist "%~dp0aliases.bat" call "%~dp0aliases.bat"
