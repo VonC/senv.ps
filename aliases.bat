69ff2b3e139b4fb48a48b74c35edfd2d7bf863a1
VonC <vonc@laposte.net>
2014-01-02 14:53:58 +0100

    Fix gocov alias

--- aliases.bat
+++ aliases.bat
@@ -6,4 +6,5 @@ doskey cdgit=cd %PROG%\git\git
 set GFW="%LOCALAPPDATA%\GitHub\GitHub.appref-ms"
 doskey g4w=%GFW% $*
 
-doskey gocov=go test -coverprofile=coverage.out^&^&go tool cover -html=coverage.out ; doskey /exename=gocov gocov=go test -coverprofile=coverage.out^&^&go tool cover -html=coverage.out
+doskey gocov=go test -coverprofile=coverage.out^&^&go tool cover -html=coverage.out
+doskey /exename=gocov gocov=go test -coverprofile=coverage.out^&^&go tool cover -html=coverage.out
