44ffb88fd56e514b3a186f35bbb3060c66c62876
VonC <vonc@laposte.net>
2014-01-02 14:57:30 +0100

    Copy aliases.bat and goupdate.bat if not exist.

--- senv.bat
+++ senv.bat
@@ -10,4 +10,7 @@ copy "%prog%\git\senv\l.bat" "%prgs%\bin" >NUL
 copy "%prog%\git\senv\kp*.bat" "%prgs%\bin" >NUL
 copy "%prog%\git\senv\gtags.bat" "%prgs%\bin" >NUL
 
+if not exist "%prgs%\bin\aliases.bat" copy "%prog%\git\senv\aliases.bat" "%prgs%\bin" >NUL
+if not exist "%prgs%\bin\goupdate.bat" copy "%prog%\git\senv\goupdate.bat" "%prgs%\bin" >NUL
+
 if exist "%~dp0aliases.bat" call "%~dp0aliases.bat"
