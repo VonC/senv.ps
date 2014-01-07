a586dbb48570bb6937d95c3bdd1668e8ee9dfe76
VonC <vonc@laposte.net>
2014-01-07 12:51:32 +0100

    Make goupdate synchrone. Fix github.com/bradfitz/goimports.

--- goupdate.bat
+++ goupdate.bat
@@ -1,3 +1,3 @@
-start /b go get -u github.com/smartystreets/goconvey
-start /b go get -u github.com/jessevdk/go-flags
-start /b go get -u https://github.com/bradfitz/goimports
+go get -u github.com/smartystreets/goconvey
+go get -u github.com/jessevdk/go-flags
+go get -u github.com/bradfitz/goimports
