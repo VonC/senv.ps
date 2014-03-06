3b7a351b8733f6ea59ff4d571367492f6ef5a747
VonC <vonc@laposte.net>
2014-03-06 10:14:45 +0100

    Fix g.bat and gi.bat by corrrectly counting parameters.

--- g.bat
+++ g.bat
@@ -1,6 +1,12 @@
-if %1=="" goto end
-if %2=="" goto have_1
-if %3=="" goto have_2
+@echo off
+rem http://stackoverflow.com/questions/1291941/batch-files-number-of-command-line-arguments
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+rem echo %argC%
+if %argC%==0 goto end
+if %argC%==1 goto have_1
+if %argC%==2 goto have_2
+goto end
 :have_1
 grep -nrHI %1 *
 goto end
